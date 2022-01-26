d06d694bb591a2ff8dfdd428998014ffc31c0ad7
GreenArt <GreenArt@DESKTOP-758VK1T>
2022-01-26 14:14:51 +0900

    헤더, 페이저 재수정

--- huggies/src/main/webapp/resources/js/container.js
+++ huggies/src/main/webapp/resources/js/container.js
@@ -2,7 +2,6 @@
 $(function(){
     $('.msImg').bxSlider({
         mode: 'horizontal',
-        pager : false,
         nextText : '<i class="fas fa-angle-right"></i>',
         prevText : '<i class="fas fa-angle-left"></i>'
     });
